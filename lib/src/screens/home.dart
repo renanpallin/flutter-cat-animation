ea3b915e0614e8c143f5a116a5be075b570cedd8
Renan Pallin <renanpallin@gmail.com>
2019-02-05 20:14:22 -0200

    Finished!

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -18,18 +18,30 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   void initState() {
     super.initState();
 
-//    boxController = Animation();
     boxController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 300),
       vsync: this,
     );
 
-    boxAnimation = Tween(begin: 0.0, end: pi).animate(
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.65,
+    ).animate(
       CurvedAnimation(
         parent: boxController,
-        curve: Curves.linear,
+        curve: Curves.easeInOut,
       ),
     );
+
+    boxController.addStatusListener((status) {
+//      print('changed: $status');
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
 
     catController = AnimationController(
       duration: Duration(milliseconds: 200),
@@ -44,12 +56,16 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
     );
   }
 
-  onTap() async {
-    if (catController.status == AnimationStatus.completed)
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      boxController.forward();
       catController.reverse();
+    }
 
-    if (catController.status == AnimationStatus.dismissed)
+    if (catController.status == AnimationStatus.dismissed) {
+      boxController.stop();
       catController.forward();
+    }
   }
 
   @override
@@ -71,6 +87,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
               buildCatAnimation(),
               buildBox(),
               buildLeftFlap(),
+              buildRightFlap(),
             ],
             overflow: Overflow.visible,
           ),
@@ -131,18 +148,43 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   }
 
   Widget buildLeftFlap() {
-    Widget flap = Container(
-      height: 10.0,
-      width: 125.0,
-      color: Colors.brown,
-    );
-
     return Positioned(
       left: 3.0,
-      child: Transform.rotate(
-        alignment: Alignment.topLeft,
-        angle: pi * .6,
-        child: flap,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value,
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            alignment: Alignment.topRight,
+            angle: -boxAnimation.value,
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.red,
+        ),
       ),
     );
   }
